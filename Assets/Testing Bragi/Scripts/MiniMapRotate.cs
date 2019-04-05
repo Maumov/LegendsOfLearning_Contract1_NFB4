9666355c7691172ae2044c38e6c2734b3043fedc
Antonio <antonyurieta@hotmail.com>
2019-04-05 11:34:37 -0500

    MiniMap script

--- Assets/Testing Bragi/Scripts/MiniMapRotate.cs
+++ Assets/Testing Bragi/Scripts/MiniMapRotate.cs
@@ -5,25 +5,13 @@ using UnityEngine;
 public class MiniMapRotate : MonoBehaviour
 {
     public Transform player;
-    public float rotationSpeed;
-    public enum RotationType { smooth, fixedRotation}
-    public RotationType rotationType = RotationType.smooth;
+    public bool isStatic = false;
 
     private void Update()
     {
-        if(rotationType == RotationType.smooth)
+        if (!isStatic)
         {
-            Vector3 playerRotation = new Vector3(0f, 0f, player.rotation.z);
-            Quaternion smooth = Quaternion.FromToRotation(transform.rotation.eulerAngles, playerRotation);
-            transform.rotation = Quaternion.Lerp(transform.rotation, smooth, rotationSpeed * Time.deltaTime);
-        }
-    }
-
-    private void LateUpdate()
-    {
-        if (rotationType == RotationType.fixedRotation)
-        {
-            transform.rotation = Quaternion.Euler(0f, 0f, player.transform.rotation.z);
+            transform.rotation = Quaternion.Euler(0f, 0f, -player.localEulerAngles.y);
         }
     }
 }
