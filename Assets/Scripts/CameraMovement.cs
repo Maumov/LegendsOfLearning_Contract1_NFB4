57e31b8cb163c39992288b21757aaea010f3f98f
Mauricio <mau.movilla@gmail.com>
2019-04-05 20:01:38 -0500

    camera and player movement

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+
+    public Transform target;
+    public Transform YRotationtransform;
+
+    float vertical;
+    float horizontal;
+
+  
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.SetParent(target);
+        transform.localPosition = Vector3.zero;
+        transform.localRotation = Quaternion.identity;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetInputs();
+
+        transform.RotateAround(transform.position, Vector3.up, horizontal);
+        YRotationtransform.RotateAround(YRotationtransform.position, YRotationtransform.right, vertical);
+    }
+    void GetInputs() {
+        vertical = Input.GetAxis("Mouse Y");
+        horizontal = Input.GetAxis("Mouse X");
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float movementSpeed = 3f;
+
+    float vertical;
+    float horizontal;
+
+    CharacterController characterController;
+    // Start is called before the first frame update
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetInputs();
+        Vector3 direction = new Vector3(horizontal,0f,vertical);
+        characterController.Move(transform.rotation * direction * movementSpeed * Time.deltaTime);
+    }
+
+    void GetInputs() {
+        vertical = Input.GetAxis("Vertical");
+        horizontal = Input.GetAxis("Horizontal");
+    }
+}
