adc253d3edbfab8cb348b63cbf7ce67296de64e5
Mauricio <mau.movilla@gmail.com>
2019-05-15 13:05:35 -0500

    fix al cofre

---
+++ Assets/Scripts/DeactivateSlot.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeactivateSlot : MonoBehaviour
+{
+    public int id;
+    void Start()
+    {
+        for(int i = 0; i < DoorsManager.completed.Count; i++)
+        {
+            if(id == DoorsManager.completed[i].id)
+            {
+                gameObject.SetActive(false);
+            }
+        }
+    }
+}
