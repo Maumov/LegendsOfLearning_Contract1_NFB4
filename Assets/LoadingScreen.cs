fb3955c1dd23319679ac0caf3b888e7092ec259d
Antonio <antonyurieta@hotmail.com>
2019-05-28 21:38:41 -0500

    Unida

--- Assets/LoadingScreen.cs
+++ Assets/LoadingScreen.cs
@@ -6,11 +6,24 @@ using UnityEngine;
 
 public class LoadingScreen : MonoBehaviour
 {
+    public GameObject mainMenuCanvas;
     public Slider slider;
 
-    IEnumerator LoadBackgroundScene()
+    public void ChangeLevel(string sceneName)
     {
-        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync("Game");
+        if(SceneManager.GetActiveScene().name == "MainMenu")
+        {
+            if(mainMenuCanvas != null)
+            {
+                mainMenuCanvas.SetActive(false);
+            }
+        }
+        StartCoroutine(LoadBackgroundScene(sceneName));
+    }
+
+    IEnumerator LoadBackgroundScene(string sceneName)
+    {
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(sceneName);
         while (!asyncLoad.isDone)
         {
             slider.value = asyncLoad.progress;
